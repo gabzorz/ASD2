68d73286ee482d0924f4d0a76a45768534a10603
HamishLamond <62045373+HamishLamond@users.noreply.github.com>
2020-09-08 14:34:07 +1000

    Created Auction_Item class

---
+++ src/java/uts/asd/model/Auction_Item.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.asd.model;
+
+/**
+ *
+ * @author Hamish Lamond
+ */
+public class Auction_Item {
+    
+    private String itemID;
+    private String propertyID;
+    private String staffUserID;
+    private String userID;
+    private String soldTo;
+    private String keywordID;
+    private String purchaseID;
+    private int soldFor;
+    private String dateVerified;
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
+    private int reservePrice;
+    private int startingPrice;
+    private String status;
+
+    public Auction_Item(String itemID, String propertyID, String staffUserID, String userID, String keywordID, String startDate, String startTime, String endDate, String endTime, int reservePrice, int startingPrice) {
+        this.itemID = itemID;
+        this.propertyID = propertyID;
+        this.staffUserID = staffUserID;
+        this.userID = userID;
+        this.keywordID = keywordID;
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
+        this.reservePrice = reservePrice;
+        this.startingPrice = startingPrice;
+        this.status = "ongoing";
+    }
+
+    // For initial testing purposes only
+    public Auction_Item(String startDate, String startTime, String endDate, String endTime, int reservePrice, int startingPrice) {
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
+        this.reservePrice = reservePrice;
+        this.startingPrice = startingPrice;
+        this.status = "ongoing";
+    }
+    
+    
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getPropertyID() {
+        return propertyID;
+    }
+
+    public void setPropertyID(String propertyID) {
+        this.propertyID = propertyID;
+    }
+
+    public String getStaffUserID() {
+        return staffUserID;
+    }
+
+    public void setStaffUserID(String staffUserID) {
+        this.staffUserID = staffUserID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getSoldTo() {
+        return soldTo;
+    }
+
+    public void setSoldTo(String soldTo) {
+        this.soldTo = soldTo;
+    }
+
+    public String getKeywordID() {
+        return keywordID;
+    }
+
+    public void setKeywordID(String keywordID) {
+        this.keywordID = keywordID;
+    }
+
+    public String getPurchaseID() {
+        return purchaseID;
+    }
+
+    public void setPurchaseID(String purchaseID) {
+        this.purchaseID = purchaseID;
+    }
+
+    public int getSoldFor() {
+        return soldFor;
+    }
+
+    public void setSoldFor(int soldFor) {
+        this.soldFor = soldFor;
+    }
+
+    public String getDateVerified() {
+        return dateVerified;
+    }
+
+    public void setDateVerified(String dateVerified) {
+        this.dateVerified = dateVerified;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getReservePrice() {
+        return reservePrice;
+    }
+
+    public void setReservePrice(int reservePrice) {
+        this.reservePrice = reservePrice;
+    }
+
+    public int getStartingPrice() {
+        return startingPrice;
+    }
+
+    public void setStartingPrice(int startingPrice) {
+        this.startingPrice = startingPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
